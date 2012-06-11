21915b130d57d6d43b4e400f8ba677c88e062d81
unknown <Adio@Adio-PC.(none)>
2012-06-11 13:29:33 -0700

    not the cell table is in a decorate Panel

--- src/com/clouway/exreport/client/expensesdashboard/view/ExpensesReporterDashboardViewImpl.java
+++ src/com/clouway/exreport/client/expensesdashboard/view/ExpensesReporterDashboardViewImpl.java
@@ -18,6 +18,7 @@ import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.cellview.client.CellTable;
 import com.google.gwt.user.cellview.client.CellTree;
 import com.google.gwt.user.cellview.client.Column;
+import com.google.gwt.user.cellview.client.SimplePager;
 import com.google.gwt.user.client.ui.HTMLPanel;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.ScrollPanel;
@@ -36,35 +37,35 @@ import java.util.Date;
  */
 public class ExpensesReporterDashboardViewImpl implements ExpenseReporterDashBoardView, TreeViewModel {
 
-  interface ExpensesReporterDashboardViewImplUiBinder extends UiBinder<HTMLPanel, ExpensesReporterDashboardViewImpl> {
+    interface ExpensesReporterDashboardViewImplUiBinder extends UiBinder<HTMLPanel, ExpensesReporterDashboardViewImpl> {
 
-  }
+    }
 
-  private AsyncDataProvider<Year> yearAsyncDataProvider;
+    private AsyncDataProvider<Year> yearAsyncDataProvider;
 
-  private AsyncDataProvider<Month> monthAsyncDataProvider;
+    private AsyncDataProvider<Month> monthAsyncDataProvider;
 
-  private AsyncDataProvider<Day> dayAsyncDataProvider;
+    private AsyncDataProvider<Day> dayAsyncDataProvider;
 
-  private static ExpensesReporterDashboardViewImplUiBinder ourUiBinder = GWT.create(ExpensesReporterDashboardViewImplUiBinder.class);
+    private static ExpensesReporterDashboardViewImplUiBinder ourUiBinder = GWT.create(ExpensesReporterDashboardViewImplUiBinder.class);
 
-  private ExpenseReporterDashboardPresenter presenter;
+    private ExpenseReporterDashboardPresenter presenter;
 
-  private ExpenseReporterServiceAsync async = GWT.create(ExpenseReporterService.class);
+    private ExpenseReporterServiceAsync async = GWT.create(ExpenseReporterService.class);
 
-  HTMLPanel maiPanel;
+    HTMLPanel maiPanel;
 
-  private Year currentYear;
+    private Year currentYear;
 
-  private Month currentMonth;
+    private Month currentMonth;
 
-  final SingleSelectionModel<Day> singleSelectionModel = new SingleSelectionModel<Day>();
+    final SingleSelectionModel<Day> singleSelectionModel = new SingleSelectionModel<Day>();
 
-  @UiField(provided = true)
-  CellTree cellTree;
+    @UiField(provided = true)
+    CellTree cellTree;
 
-  @UiField
-  CellTable<Expense> expensesCellTable;
+    @UiField
+    CellTable<Expense> expensesCellTable;
 
     @UiField
     ScrollPanel cellTreeScrollPanel;
@@ -75,135 +76,134 @@ public class ExpensesReporterDashboardViewImpl implements ExpenseReporterDashBoa
 
     public ExpensesReporterDashboardViewImpl() {
 
-    presenter = new ExpenseReporterDashboardPresenter(this, async);
+        presenter = new ExpenseReporterDashboardPresenter(this, async);
 
-    singleSelectionModel.addSelectionChangeHandler(new SelectionChangeEvent.Handler() {
+        singleSelectionModel.addSelectionChangeHandler(new SelectionChangeEvent.Handler() {
 
-      @Override
-      public void onSelectionChange(SelectionChangeEvent event) {
+            @Override
+            public void onSelectionChange(SelectionChangeEvent event) {
 
-        Day day = singleSelectionModel.getSelectedObject();
+                Day day = singleSelectionModel.getSelectedObject();
 
-        DateTimeFormat dateTimeFormat = DateTimeFormat.getFormat("YYYY/MM/DD");
+                DateTimeFormat dateTimeFormat = DateTimeFormat.getFormat("YYYY/MM/DD");
 
-        Date date = new Date(day.getDay(), day.getMonth(), day.getYear());
+                Date date = new Date(day.getDay(), day.getMonth(), day.getYear());
 
-        presenter.showExpensesFor(date);
+                presenter.showExpensesFor(date);
 
-      }
-    });
+            }
+        });
 
-    yearAsyncDataProvider = new AsyncDataProvider<Year>() {
-      @Override
-      protected void onRangeChanged(HasData<Year> display) {
-        presenter.getAllExpensesYears();
-      }
-    };
+        yearAsyncDataProvider = new AsyncDataProvider<Year>() {
+            @Override
+            protected void onRangeChanged(HasData<Year> display) {
+                presenter.getAllExpensesYears();
+            }
+        };
 
-    monthAsyncDataProvider = new AsyncDataProvider<Month>() {
-      @Override
-      protected void onRangeChanged(HasData<Month> display) {
-        presenter.getMonthsOf(currentYear.getYear());
-      }
-    };
+        monthAsyncDataProvider = new AsyncDataProvider<Month>() {
+            @Override
+            protected void onRangeChanged(HasData<Month> display) {
+                presenter.getMonthsOf(currentYear.getYear());
+            }
+        };
 
-    dayAsyncDataProvider = new AsyncDataProvider<Day>() {
-      @Override
-      protected void onRangeChanged(HasData<Day> display) {
-        presenter.getAllExpensesDays(currentYear.getYear(), currentMonth.getMonth());
-      }
-    };
-
-
-    cellTree = new CellTree(this, null);
+        dayAsyncDataProvider = new AsyncDataProvider<Day>() {
+            @Override
+            protected void onRangeChanged(HasData<Day> display) {
+                presenter.getAllExpensesDays(currentYear.getYear(), currentMonth.getMonth());
+            }
+        };
 
 
+        cellTree = new CellTree(this, null);
+        SimplePager.Resources pagerResources = GWT.create(SimplePager.Resources.class);
+        maiPanel = ourUiBinder.createAndBindUi(this);
 
+        expensesCellTable.addColumn(new Column<Expense, String>(new TextCell()) {
+            @Override
+            public String getValue(Expense object) {
+                return object.getName();
+            }
+        });
 
-
-    maiPanel = ourUiBinder.createAndBindUi(this);
-    cellTreePanel.add(cellTree);
-    expensesCellTable.addColumn(new Column<Expense, String>(new TextCell()) {
-      @Override
-      public String getValue(Expense object) {
-        return object.getName();
-      }
-    });
-
-    expensesCellTable.addColumn(new Column<Expense, String>(new TextCell()) {
-      @Override
-      public String getValue(Expense object) {
-        return String.valueOf(object.getPrice());
-      }
-    });
-  }
-
-  @Override
-  public <T> NodeInfo<?> getNodeInfo(T value) {
-    if (value == null) {
-      return new DefaultNodeInfo<Year>(yearAsyncDataProvider, new YearCell());
+        expensesCellTable.addColumn(new Column<Expense, String>(new TextCell()) {
+            @Override
+            public String getValue(Expense object) {
+                return String.valueOf(object.getPrice());
+            }
+        });
     }
 
-    if (value instanceof Year) {
-      currentYear = (Year) value;
-      return new DefaultNodeInfo<Month>(monthAsyncDataProvider, new MonthCell());
+    @Override
+    public <T> NodeInfo<?> getNodeInfo(T value) {
+        if (value == null) {
+            return new DefaultNodeInfo<Year>(yearAsyncDataProvider, new YearCell());
+        }
+
+        if (value instanceof Year) {
+            currentYear = (Year) value;
+            return new DefaultNodeInfo<Month>(monthAsyncDataProvider, new MonthCell());
+        }
+        if (value instanceof Month) {
+            currentMonth = (Month) value;
+            return new DefaultNodeInfo<Day>(dayAsyncDataProvider, new DayCell(), singleSelectionModel, null);
+        }
+        return null;
     }
-    if (value instanceof Month) {
-      currentMonth = (Month) value;
-      return new DefaultNodeInfo<Day>(dayAsyncDataProvider, new DayCell(), singleSelectionModel, null);
+
+    @Override
+    public boolean isLeaf(Object value) {
+        return value instanceof Day;
     }
-    return null;
-  }
-
-  @Override
-  public boolean isLeaf(Object value) {
-    return value instanceof Day;
-  }
 
 
-  @Override
-  public void updateExpenses(ArrayList<Expense> expenses) {
-    expensesCellTable.setRowData(0, expenses);
-  }
+    @Override
+    public void updateExpenses(ArrayList<Expense> expenses) {
+        expensesCellTable.setVisibleRange(0, expenses.size());
+        expensesCellTable.setRowData(0, expenses);
+    }
 
 
-  @Override
-  public void notifyUserOfFutureDate() {
+    @Override
+    public void notifyUserOfFutureDate() {
 
-  }
+    }
 
-  @Override
-  public void notifyUserOfDateDiscrepancy() {
+    @Override
+    public void notifyUserOfDateDiscrepancy() {
 
-  }
+    }
 
-  @Override
-  public void showConnectionErrorMessage() {
+    @Override
+    public void showConnectionErrorMessage() {
 
-  }
+    }
 
-  @Override
-  public Widget asWidget() {
-    return maiPanel;
-  }
+    @Override
+    public Widget asWidget() {
+        return maiPanel;
+    }
 
-  @Override
-  public void showExpensesYears(ArrayList<Year> yearList) {
-    yearAsyncDataProvider.updateRowCount(yearList.size(), true);
-    yearAsyncDataProvider.updateRowData(0, yearList);
-  }
+    @Override
+    public void showExpensesYears(ArrayList<Year> yearList) {
+        yearAsyncDataProvider.updateRowCount(yearList.size(), true);
+        yearAsyncDataProvider.updateRowData(0, yearList);
 
-  @Override
-  public void showMonthsOfExpenses(ArrayList<Month> months) {
-    monthAsyncDataProvider.updateRowCount(months.size(), true);
-    monthAsyncDataProvider.updateRowData(0, months);
-  }
+    }
 
-  @Override
-  public void showDaysExpenses(ArrayList<Day> days) {
-    dayAsyncDataProvider.updateRowCount(days.size(), true);
-    dayAsyncDataProvider.updateRowData(0, days);
-  }
+    @Override
+    public void showMonthsOfExpenses(ArrayList<Month> months) {
+        monthAsyncDataProvider.updateRowCount(months.size(), true);
+        monthAsyncDataProvider.updateRowData(0, months);
+    }
+
+    @Override
+    public void showDaysExpenses(ArrayList<Day> days) {
+        dayAsyncDataProvider.updateRowCount(days.size(), true);
+        dayAsyncDataProvider.updateRowData(0, days);
+
+    }
 
 
 }
